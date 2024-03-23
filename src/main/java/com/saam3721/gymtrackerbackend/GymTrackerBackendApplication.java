ad5725f12c3bb9e17d20eba5fdee96eedb3d2678
Saam <sazodev@proton.me>
2024-03-23 13:03:24 +0100

    Initial commit

---
+++ src/main/java/com/saam3721/gymtrackerbackend/GymTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saam3721.gymtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saam3721/gymtrackerbackend/GymTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saam3721.gymtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
