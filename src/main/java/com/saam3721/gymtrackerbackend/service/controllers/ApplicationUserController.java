15058b3c7900019731ae1b791700340f71661839
Saam <sazodev@proton.me>
2024-03-26 11:13:55 +0100

    Add basic REST controller for user registration

---
+++ src/main/java/com/saam3721/gymtrackerbackend/service/controllers/ApplicationUserController.java
@@ -0,0 +1,30 @@
+package com.saam3721.gymtrackerbackend.service.controllers;
+
+import com.saam3721.gymtrackerbackend.service.entities.ApplicationUser;
+import com.saam3721.gymtrackerbackend.service.repositories.ApplicationUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+@AllArgsConstructor
+public class ApplicationUserController {
+
+    private ApplicationUserRepository applicationUserRepository;
+
+
+    @PostMapping("register")
+    public ResponseEntity<?> createUser(@RequestBody ApplicationUser newUser) {
+        try {
+            applicationUserRepository.save(newUser); // TODO: Hash the password before saving it
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("An error occurred while creating the user.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
