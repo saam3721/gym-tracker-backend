7d8565c5544eeabf6f8eba8263142b8eb3b38532
Saam <sazodev@proton.me>
2024-04-06 14:59:20 +0200

    Add query to find user in database

--- src/main/java/com/saam3721/gymtrackerbackend/repositories/ApplicationUserRepository.java
+++ src/main/java/com/saam3721/gymtrackerbackend/repositories/ApplicationUserRepository.java
@@ -4,8 +4,12 @@ import com.saam3721.gymtrackerbackend.entities.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, UUID> {
+
+    Optional<ApplicationUser> findByUsername(String username);
+
 }
