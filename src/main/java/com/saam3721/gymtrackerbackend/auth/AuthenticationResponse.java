e5c1ebfdbe10037cdc1f2dd509bd8aa83300d0e0
Saam <sazodev@proton.me>
2024-04-06 15:06:59 +0200

    Implement user registration and authentication via REST controller

---
+++ src/main/java/com/saam3721/gymtrackerbackend/auth/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.saam3721.gymtrackerbackend.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+
+}
---
+++ src/main/java/com/saam3721/gymtrackerbackend/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.saam3721.gymtrackerbackend.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String username;
+
+    private String password;
+
+}
