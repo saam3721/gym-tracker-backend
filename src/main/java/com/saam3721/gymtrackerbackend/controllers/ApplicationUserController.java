764f66461662959635a9fb6d6d9ca9c9729edc73
Saam <sazodev@proton.me>
2024-04-19 16:33:09 +0200

    Add endpoint for retrieving user's username

--- src/main/java/com/saam3721/gymtrackerbackend/controllers/ApplicationUserController.java
+++ src/main/java/com/saam3721/gymtrackerbackend/controllers/ApplicationUserController.java
@@ -1,30 +1,19 @@
 package com.saam3721.gymtrackerbackend.controllers;
 
-import com.saam3721.gymtrackerbackend.entities.ApplicationUser;
-import com.saam3721.gymtrackerbackend.repositories.ApplicationUserRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/users")
 @RestController
-@AllArgsConstructor
 public class ApplicationUserController {
 
-    private ApplicationUserRepository applicationUserRepository;
-
-
-    @PostMapping("register")
-    public ResponseEntity<?> createUser(@RequestBody ApplicationUser newUser) {
-        try {
-            applicationUserRepository.save(newUser); // TODO: Hash the password before saving it
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>("An error occurred while creating the user.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @GetMapping("me")
+    public ResponseEntity<String> me() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return new ResponseEntity<>(username, HttpStatus.OK);
     }
 }
